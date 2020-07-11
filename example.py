c043e11c3fe33ec177878c4fee8d541220140e96
jurgendn <nguyentiendung7299@gmail.com>
2020-07-11 07:30:32 +0700

    Update import

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
-from PHMMs import *
+import PHMMs as pm
+import numpy as np
 
 theta = np.array([[0.6, 0.4], [0.5, 0.5]])
 delta = np.array([0.8, 0.2])
 lambdas = np.array([0.3, 10])
 seqs = [0, 3, 2, 9, 9, 0, 1, 1, 1, 4, 5, 1, 1, 1, 1, 0, 2, 3, 2, 3, 1, 5, 5, 4, 6, 1, 6, 1, 1, 1, 5, 1, 5, 0, 8, 1, 4, 4, 2, 2, 2, 0, 8, 1, 2, 6, 0, 7, 8, 7,
         2, 3, 2, 10, 1, 7, 1, 4, 2, 1, 8, 1, 2, 10, 5, 4, 2, 6, 0, 2, 2, 8, 5, 5, 8, 6, 2, 5, 1, 1, 0, 3, 6, 2, 0, 9, 0, 1, 0, 8, 13, 10, 2, 1, 2, 3, 9, 4, 3, 5]
-test = PHMMs(delta, theta, lambdas, seqs, epsi=0.0001)
+test = pm.PHMMs(delta, theta, lambdas, seqs, epsi=0.0001)
 test.Baum_Welch()
 print("_________________")
 print("_Infomation Test_")
